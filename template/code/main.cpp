9e3be074e60c3bd8ada1f571208fe71331bf5553
Arc Chang <f22893837@gmail.com>
2017-12-15 18:01:37 +0800

    version 3

--- template/code/main.cpp
+++ template/code/main.cpp
@@ -7,10 +7,9 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 
-#define GAME_TERMINATE 3
+#define GAME_TERMINATE -1
 
-const float FPS = 60;
-
+// ALLEGRO Variables
 ALLEGRO_DISPLAY* display = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 ALLEGRO_BITMAP *image = NULL;
@@ -23,27 +22,30 @@ ALLEGRO_TIMER *timer2 = NULL;
 ALLEGRO_SAMPLE *song=NULL;
 ALLEGRO_FONT *font = NULL;
 
-bool transfer ;
-
-const int width = 800;
-const int height = 600;
-
+//Custom Definition
+const char *title = "Final Project 10xxxxxxx";
+const float FPS = 60;
+const int WIDTH = 800;
+const int HEIGHT = 600;
 typedef struct character
 {
     int x;
     int y;
+    ALLEGRO_BITMAP *image_path;
 
 }Character;
 
 Character character1;
 Character character2;
+Character character3;
 
 int imageWidth = 0;
 int imageHeight = 0;
-int draw = 0 ;
-int done = 0 ;
+int draw = 0;
+int done = 0;
 int window = 1;
 bool judge_next_window = false;
+bool ture_1 , ture_2;
 
 void show_err_msg(int msg);
 void game_init();
@@ -51,22 +53,19 @@ void game_begin();
 int process_event();
 int game_run();
 void game_destroy();
-bool ture_1 , ture_2 ;
-
 
 int main(int argc, char *argv[]) {
     int msg = 0;
 
     game_init();
-
     game_begin();
-
 
     while (msg != GAME_TERMINATE) {
         msg = game_run();
         if (msg == GAME_TERMINATE)
             printf("Game Over\n");
     }
+
     game_destroy();
     return 0;
 }
@@ -93,116 +92,99 @@ void game_init() {
         fprintf(stderr, "failed to reserve samples!\n");
         show_err_msg(-1);
     }
-    song = al_load_sample( "hello.wav" );
-    if (!song){
-        printf( "Audio clip sample not loaded!\n" );
-        show_err_msg(-1);
-    }
-
-    display = al_create_display(width, height);
+    // Create display
+    display = al_create_display(WIDTH, HEIGHT);
     event_queue = al_create_event_queue();
-
     if (display == NULL || event_queue == NULL) {
         show_err_msg(-1);
     }
-
-    character1.x = width / 2 - 100;
-    character1.y = height / 2 - 100;
-    character2.x = width / 2 + 100;
-    character2.y = height / 2 + 100;
-
-    al_set_window_position( display, 0, 0);
-    const char *title = "Final Project 10xxxxxxx";
-    al_set_window_title( display, title);
-
+    // Initialize Allegro settings
+    al_set_window_position(display, 0, 0);
+    al_set_window_title(display, title);
     al_init_primitives_addon();
     al_install_keyboard();
     al_install_audio();
     al_init_image_addon();
     al_init_acodec_addon();
-    al_init_font_addon(); // initialize the font addon
-    al_init_ttf_addon(); // initialize the ttf (True Type Font) addon
+    al_init_font_addon();
+    al_init_ttf_addon();
 
-
-    image = al_load_bitmap("teemo.png");
-    image2 = al_load_bitmap("maokai.png");
-    image3 = al_load_bitmap("azir.png");
-
-    background = al_load_bitmap("stage.jpg");
-
-    font = al_load_ttf_font("pirulen.ttf",12,0); // load font
-
+    // Register event
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_keyboard_event_source());
-    al_reserve_samples(10);
-
 }
 
 void game_begin() {
-    //Loop the song until the display closes
+    // Load sound
+    song = al_load_sample( "hello.wav" );
+    if (!song){
+        printf( "Audio clip sample not loaded!\n" );
+        show_err_msg(-1);
+    }
+    // Loop the song until the display closes
     al_play_sample(song, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_LOOP,NULL);
     al_clear_to_color(al_map_rgb(100,100,100));
-    al_draw_text(font, al_map_rgb(255,255,255), 400, 520 , ALLEGRO_ALIGN_CENTRE, "Press 'Enter' to start");
+    // Load and draw text
+    font = al_load_ttf_font("pirulen.ttf",12,0);
+    al_draw_text(font, al_map_rgb(255,255,255), WIDTH/2, HEIGHT/2+220 , ALLEGRO_ALIGN_CENTRE, "Press 'Enter' to start");
     al_draw_rectangle(200, 510, 600, 550, al_map_rgb(255, 255, 255), 0);
     al_flip_display();
-
 }
 
-/* For Keyboard */
 int process_event(){
+    // Request the event
     ALLEGRO_EVENT event;
     al_wait_for_event(event_queue, &event);
 
-
+    // Our setting for controlling animation
     if(event.timer.source == timer){
         ture_1 = !ture_1 ;
-
     }
-
     if(event.timer.source == timer2){
         ture_2 = !ture_2 ;
     }
 
-
+    // Keyboard
     if(event.type == ALLEGRO_EVENT_KEY_UP)
     {
         switch(event.keyboard.keycode)
         {
-            // p1 control
-            case ALLEGRO_KEY_UP:
+            // P1 control
+            case ALLEGRO_KEY_W:
                 character1.y -= 30;
                 break;
-            case ALLEGRO_KEY_DOWN:
+            case ALLEGRO_KEY_S:
                 character1.y += 30;
                 break;
-            case ALLEGRO_KEY_RIGHT:
-                character1.x += 30;
-                break;
-            case ALLEGRO_KEY_LEFT:
+            case ALLEGRO_KEY_A:
                 character1.x -= 30;
                 break;
+            case ALLEGRO_KEY_D:
+                character1.x += 30;
+                break;
 
-             //p2 control
-            case ALLEGRO_KEY_W:
+            // P2 control
+            case ALLEGRO_KEY_UP:
                 character2.y -= 30;
                 break;
-            case ALLEGRO_KEY_S:
+            case ALLEGRO_KEY_DOWN:
                 character2.y += 30;
                 break;
-            case ALLEGRO_KEY_A:
-                character2.x -= 30;
-                break;
-            case ALLEGRO_KEY_D:
+            case ALLEGRO_KEY_RIGHT:
                 character2.x += 30;
                 break;
+            case ALLEGRO_KEY_LEFT:
+                character2.x -= 30;
+                break;
 
+            // For Start Menu
             case ALLEGRO_KEY_ENTER:
                 judge_next_window = true;
                 break;
         }
-
     }
 
+    // Shutdown our program
     else if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         return GAME_TERMINATE;
 
@@ -211,11 +193,22 @@ int process_event(){
 
 int game_run() {
     int error = 0;
+    // First window(Menu)
     if(window == 1){
         if (!al_is_event_queue_empty(event_queue)) {
             error = process_event();
             if(judge_next_window) {
                 window = 2;
+                // Setting Character
+                character1.x = WIDTH / 2 - 300;
+                character1.y = HEIGHT / 2 + 75;
+                character2.x = WIDTH / 2 + 170;
+                character2.y = HEIGHT / 2 + 100;
+                character1.image_path = al_load_bitmap("maokai.png");
+                character2.image_path= al_load_bitmap("teemo.png");
+                character3.image_path = al_load_bitmap("Azir.png");
+                background = al_load_bitmap("stage.jpg");
+
                 //Initialize Timer
                 timer  = al_create_timer(1.0);
                 timer2  = al_create_timer(1.0/3.0);
@@ -226,14 +219,17 @@ int game_run() {
             }
         }
     }
+    // Second window(Main Game)
     else if(window == 2){
+        // Change Image for animation
         al_draw_bitmap(background, 0,0, 0);
-        if(ture_1)al_draw_bitmap(image2, character1.x, character1.y, 0);
-        if(ture_2)al_draw_bitmap(image, character2.x, character2.y, 0);
-        else al_draw_bitmap(image3, character2.x, character2.y, 0);
-
+        if(ture_1)al_draw_bitmap(character1.image_path, character1.x, character1.y, 0);
+        if(ture_2)al_draw_bitmap(character2.image_path, character2.x, character2.y, 0);
+        else al_draw_bitmap(character3.image_path, character2.x, character2.y, 0);
         al_flip_display();
         al_clear_to_color(al_map_rgb(0,0,0));
+
+        // Listening for new event
         if (!al_is_event_queue_empty(event_queue)) {
             error = process_event();
         }
@@ -242,6 +238,7 @@ int game_run() {
 }
 
 void game_destroy() {
+    // Make sure you destroy all things
     al_destroy_event_queue(event_queue);
     al_destroy_display(display);
     al_destroy_timer(timer);
